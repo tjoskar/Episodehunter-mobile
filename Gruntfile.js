7536d66552e952b28ab9a6240e8d92328fbf5389
Oskar Karlsson <mr.karlsson.oskar@gmail.com>
2014-09-25 21:09:21 +0200

    Remove phonegap file and install lineman

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,14 @@
-/* global module */
+/* global module:false */
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*\n<%= pkg.name %> - v<%= pkg.version %> - ' +
-          '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-          '<%= pkg.homepage %>\n' +
-          'Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.name %>\n*/\n',
+    // grunt.initConfig({
+    //     pkg: grunt.file.readJSON('package.json'),
+    //     banner: '/*\n<%= pkg.name %> - v<%= pkg.version %> - ' +
+    //           '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+    //           '<%= pkg.homepage %>\n' +
+    //           'Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.name %>\n*/\n',
+    // });
 
-    jshint: {
-      gruntfile: {
-        options: {
-          jshintrc: '.jshintrc'
-        },
-        src: 'Gruntfile.js'
-      },
-      script: {
-        options: {
-          jshintrc: '.jshintrc'
-        },
-        src: [
-          'js/*.js',
-          'js/controller/*.js',
-          'js/lib/*.js',
-          'js/model/*.js',
-          'js/repositories/*.js',
-          'tests/*.js'
-        ]
-      },
-    },
-
-    karma: {
-      unit: {
-        configFile: 'tests/karma.config.js'
-      }
-    }
-  });
-
-
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-karma');
-
-  // Default task(s).
-  grunt.registerTask('default', ['jshint', 'karma']);
-  grunt.registerTask('test', ['karma']);
+    require('./config/lineman').config.grunt.run(grunt);
 
 };
--- README.md
+++ README.md
@@ -1,20 +1,13 @@
-Episodehunter Mobile [![Build Status](https://travis-ci.org/tjoskar/Episodehunter-mobile.png?branch=master)](https://travis-ci.org/tjoskar/Episodehunter-mobile)
+Episodehunter Mobile [![Build Status](https://travis-ci.org/tjoskar/Episodehunter-mobile.png?branch=convert-to-lineman-project)](https://travis-ci.org/tjoskar/Episodehunter-mobile)
 ===================
 
 A complementary APP for EpisodeHunter.tv
 
-This app is written in javascript and HTML with [PhoneGap](http://phonegap.com/) and [Angular](http://angularjs.org/).
+This app is written in JavaScript and HTML with [Lineman](http://www.linemanjs.com/) and [Angular](http://angularjs.org/).
 
-###Wanna help?
+### Wanna help? - Great!
 1. Fork this repo
 2. Clone your fork of the repo: `git clone https://github.com/<username>/Episodehunter-mobile.git`
 3. Add an "upstream" remote: `git remote add upstream https://github.com/tjoskar/Episodehunter-mobile.git`
 4. Install packet dependency: `npm install`
-
-####Build it for ios
-1. Create a cordova project `cordova create . tv.mobie.episodehunter EH`
-2. Replace the www folder with this repo.
-3. Add a platform `cordova platform add ios`
-4. Build that thing `cordova build ios`
-5. Launch the iOS simulator `cordova emulate ios`
-6. Contact me at info@episodehunter.tv
+5. Run it: `lineman start`
